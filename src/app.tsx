d7129d6f8a411d793d3172ffdab781e6fe3f1cca
Gabriel de Melo Marcondes <gabriel-santos1313@hotmail.com>
2024-05-14 15:37:08 -0300

    :card_file_box: raw: change from Tags to Products

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
 import { FileDown, Filter, Loader2, MoreHorizontal, Plus, Search } from 'lucide-react'
 import { keepPreviousData, useQuery, useQueryClient } from '@tanstack/react-query'
@@ -7,29 +7,30 @@ import * as Dialog from '@radix-ui/react-dialog'
 
 // import useDebounceValue from './hooks/use-debounce-value'
 
-import { Header } from './components/header'
-import { Tabs } from './components/tabs'
+import { Format } from './utilities/format'
+
 import { Button } from './components/ui/button'
 import { Control, Input } from './components/ui/input'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from './components/ui/table'
 import { Pagination } from './components/pagination'
-import { CreateTagForm } from './components/create-tag-form'
+import { CreateProductForm } from './components/create-product-form'
 
-interface ITag {
-  title: string
-  slug: string
-  amountOfVideos: number
+interface IProduct {
   id: string
+  name: string
+  slug: string
+  amount: number
+  description: number
 }
 
-interface ITagResponse {
+interface IProductResponse {
   first: number
   prev: number | null
   next: number | null
   last: number
   pages: number
   items: number
-  data: ITag[]
+  data: IProduct[]
 }
 
 export function App() {
@@ -41,10 +42,12 @@ export function App() {
   const page = Number(searchParams.get('page') || 1)
   const perPage = Number(searchParams.get('per_page') || 10)
 
-  const { data: tagsResponse, isLoading, isLoadingError } = useQuery<ITagResponse>({
-    queryKey: ['get-tags', urlFilter, page, perPage],
+  const format = new Format()
+
+  const { data: productsResponse, isLoading, isLoadingError } = useQuery<IProductResponse>({
+    queryKey: ['get-products', urlFilter, page, perPage],
     queryFn: async () => {
-      const response = await fetch(`http://localhost:3333/tags?_page=${page}&_per_page=${perPage}&title=${urlFilter}`)
+      const response = await fetch(`http://localhost:3333/products?_page=${page}&_per_page=${perPage}&name=${urlFilter}`)
       const data = await response.json()
 
       // delay de 2s
@@ -56,7 +59,9 @@ export function App() {
     staleTime: 1000 * 60, // quanto tempo os dados valem, após esse tempo, o useQuery fará a requisição novamente
   })
 
-  function handleFilter() {
+  function handleFilter(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+
     setSearchParams(params => {
       params.set('page', '1')
       params.set('filter', filter)
@@ -66,13 +71,9 @@ export function App() {
 
   function handleReloadQuery() {
     queryClient.invalidateQueries({
-      queryKey: ['get-tags']
+      queryKey: ['get-products']
     })
   }
-
-  useEffect(() => {
-    console.log('perPage:', perPage)
-  }, [perPage])
 
   if (isLoading) {
     return (
@@ -83,119 +84,108 @@ export function App() {
   }
 
   return (
-    <div className="py-10 space-y-8">
-      <div>
-        <Header />
-        <Tabs />
-      </div>
-      <main className="max-w-6xl mx-auto space-y-5">
-        {tagsResponse && !isLoadingError ? (
-          <>
-            <div className="flex items-center gap-3">
-              <h1 className="text-xl font-bold">Tags</h1>
-              <Dialog.Root>
-                <Dialog.Trigger asChild>
-                  <Button variant='primary'>
-                    <Plus className="size-3" />
-                    create new
-                  </Button>
-                </Dialog.Trigger>
-
-                <Dialog.Portal>
-                  <Dialog.Overlay className='fixed inset-0 bg-black/70' />
-                  <Dialog.Content className='fixed p-10 space-y-3 right-0 top-0 bottom-0 h-screen min-w-[320px] z-10 bg-zinc-950 border-l border-zinc-900'>
-                    <div className="space-y-3">
-                      <Dialog.Title className='text-xl font-bold'>
-                        Create tag
-                      </Dialog.Title>
-
-                      <Dialog.Description className='text-sm text-zinc-500'>
-                        Tags can be used to group videos about similar concepts
-                      </Dialog.Description>
-                    </div>
-
-                    {/* FORM */}
-                    <CreateTagForm />
-                  </Dialog.Content>
-                </Dialog.Portal>
-              </Dialog.Root>
-            </div>
-
-            <div className='flex items-center justify-between'>
-              <div className="flex items-center gap-2">
-                <Input variant='filter'>
-                  <Search className='size-3' />
-                  <Control
-                    placeholder='search tags...'
-                    value={filter}
-                    onChange={e => setFilter(e.target.value)}
-                  />
-                </Input>
-
-                <Button onClick={handleFilter}>
-                  <Filter className='size-3' />
-                  Apply filters
+    <main className="max-w-6xl mx-auto py-10 px-4 space-y-5">
+      {productsResponse && !isLoadingError ? (
+        <>
+          <div className="flex items-center gap-3">
+            <h1 className="text-xl font-bold">Products</h1>
+            <Dialog.Root>
+              <Dialog.Trigger asChild>
+                <Button variant='primary'>
+                  <Plus className="size-3" />
+                  create new
                 </Button>
-              </div>
+              </Dialog.Trigger>
 
-              <Button>
-                <FileDown className='size-3' />
-                Export
+              <Dialog.Portal>
+                <Dialog.Overlay className='fixed inset-0 bg-black/70' />
+                <Dialog.Content className='w-[450px] h-screen overflow-auto fixed p-10 space-y-3 right-0 top-0 bottom-0 z-10 bg-zinc-950 border-l border-zinc-900'>
+
+                  {/* FORM */}
+                  <CreateProductForm />
+                </Dialog.Content>
+              </Dialog.Portal>
+            </Dialog.Root>
+          </div>
+
+          <div className='flex items-center justify-between'>
+            <form onSubmit={e => handleFilter(e)} className="flex items-center gap-2">
+              <Input variant='filter'>
+                <Search className='size-3' />
+                <Control
+                  placeholder='search products...'
+                  value={filter}
+                  onChange={e => setFilter(e.target.value)}
+                />
+              </Input>
+
+              <Button type='submit'>
+                <Filter className='size-3' />
+                Apply filters
               </Button>
-            </div>
+            </form>
 
-            <Pagination pages={tagsResponse.pages} items={tagsResponse.items} itemsShown={tagsResponse.data.length} page={page} perPage={perPage} />
+            <Button>
+              <FileDown className='size-3' />
+              Export
+            </Button>
+          </div>
 
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead></TableHead>
-                  <TableHead>Tag</TableHead>
-                  <TableHead>Amount of videos</TableHead>
-                  <TableHead></TableHead>
-                </TableRow>
-              </TableHeader>
+          <Pagination pages={productsResponse.pages} items={productsResponse.items} itemsShown={productsResponse.data.length} page={page} perPage={perPage} />
 
-              <TableBody>
-                {tagsResponse.data.map(tag => {
-                  return (
-                    <TableRow key={tag.id}>
-                      <TableCell></TableCell>
-                      <TableCell>
-                        <div className='flex flex-col gap-0.5'>
-                          <span className='font-medium'>{tag.title}</span>
-                          <span className='text-xs text-zinc-500'>{tag.slug}</span>
-                        </div>
-                      </TableCell>
-                      <TableCell className='text-zinc-300'>
-                        {tag.amountOfVideos} videos
-                      </TableCell>
-                      <TableCell className='text-right'>
-                        <Button size='icon'>
-                          <MoreHorizontal className='size-4' />
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  )
-                })}
-              </TableBody>
-            </Table>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead></TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Amount</TableHead>
+                <TableHead></TableHead>
+              </TableRow>
+            </TableHeader>
 
-            <Pagination pages={tagsResponse.pages} items={tagsResponse.items} itemsShown={tagsResponse.data.length} page={page} perPage={perPage} />
-          </>
-        ) : (
-          <>
-            <div className='space-y-3 text-center'>
-              <p className='text-red-400'>
-                Erro ao tentar buscar as informações.
-              </p>
-              <Button onClick={handleReloadQuery}>
-                Recarregar
-              </Button>
-            </div>
-          </>
-        )}
-      </main>
-    </div>
+            <TableBody>
+              {productsResponse.data.map(p => {
+                return (
+                  <TableRow key={p.id}>
+                    <TableCell></TableCell>
+                    <TableCell>
+                      <div className='flex flex-col gap-0.5'>
+                        <span className='font-medium'>
+                          <a href={`/product/${p.slug}`} className='hover:underline'>{p.name}</a>
+                        </span>
+                        <span className='text-xs text-zinc-500'>
+                          <a href={`/product/${p.slug}`} className='hover:underline'>{p.slug}</a>
+                        </span>
+                      </div>
+                    </TableCell>
+                    <TableCell className='text-zinc-300'>
+                      {format.currency(p.amount, true)}
+                    </TableCell>
+                    <TableCell className='text-right'>
+                      <Button size='icon'>
+                        <MoreHorizontal className='size-4' />
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+
+          <Pagination pages={productsResponse.pages} items={productsResponse.items} itemsShown={productsResponse.data.length} page={page} perPage={perPage} />
+        </>
+      ) : (
+        <>
+          <div className='space-y-3 text-center'>
+            <p className='text-red-400'>
+              Erro ao tentar buscar as informações.
+            </p>
+            <Button onClick={handleReloadQuery}>
+              Recarregar
+            </Button>
+          </div>
+        </>
+      )}
+    </main>
   )
 }
---
+++ src/components/create-product-form.tsx
@@ -0,0 +1,175 @@
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { Check, Loader2, X } from "lucide-react";
+import * as Dialog from '@radix-ui/react-dialog'
+
+import generateSlug from '../utilities/slug';
+import { Format } from '../utilities/format';
+
+import { Button } from "./ui/button";
+
+const createProductSchema = z.object({
+  name: z.string()
+    .trim()
+    .min(3, { message: 'Minimum 3 characters.' })
+    .max(50, 'Maximum 50 characters.'),
+  amount: z.coerce.number({
+    required_error: 'teste teste teste.',
+    invalid_type_error: 'Enter a value.',
+  })
+    .nonnegative('It must be positive')
+    .gt(0, 'Must be greater than 0 (zero).'),
+  description: z.string()
+    .trim()
+    .min(10, { message: 'Minimum 10 characters.' })
+    .max(200, 'Maximum 200 characters.'),
+})
+
+type CreateProductSchema = z.infer<typeof createProductSchema>
+
+export function CreateProductForm() {
+  const format = new Format()
+
+  const queryClient = useQueryClient()
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState,
+    control,
+  } = useForm<CreateProductSchema>({
+    resolver: zodResolver(createProductSchema)
+  })
+
+  const slug = watch('name')
+    ? generateSlug(watch('name'))
+    : ''
+
+  const { mutateAsync } = useMutation({
+    mutationFn: async ({ name, amount, description }: CreateProductSchema) => {
+      console.log(`{name: ${name.trim()}, amount: ${amount}, description: ${description.trim()}}`)
+      alert('foi')
+      return
+      // delay de 2s
+      await new Promise(resolve => setTimeout(resolve, 2000))
+
+      await fetch('http://localhost:3333/products', {
+        method: 'POST',
+        body: JSON.stringify({
+          name: name.trim(),
+          slug,
+          amount,
+          description: description.trim(),
+        })
+      })
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['get-products']
+      })
+    }
+  })
+
+  async function createProduct({ name, amount, description }: CreateProductSchema) {
+    await mutateAsync({ name, amount, description })
+  }
+
+  return (
+    <>
+      <div className="space-y-3">
+        <Dialog.Title className='text-xl font-bold'>
+          Create product
+        </Dialog.Title>
+
+        <Dialog.Description className='text-sm text-zinc-500'>
+          A product access link will be created automatically:
+        </Dialog.Description>
+
+        <p className='text-sm'>
+          {/* {window.location.href.slice(0, window.location.href.indexOf('?'))}/product/{slug} */}
+          {window.location.protocol}//{window.location.hostname}:{window.location.port}/product/{slug}
+        </p>
+      </div>
+
+      <form onSubmit={handleSubmit(createProduct)} className="w-full space-y-6">
+        <div className='space-y-2'>
+          <label className="block text-sm font-medium" htmlFor="name">Product name</label>
+          <input
+            {...register('name')}
+            type="text"
+            name="name"
+            id="name"
+            maxLength={50}
+            className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm"
+          />
+          {formState.errors.name && (
+            <p className='text-sm text-red-400'>{formState.errors.name.message}</p>
+          )}
+        </div>
+
+        <div className='space-y-2'>
+          <label className="block text-sm font-medium" htmlFor="amount">Amount</label>
+          <Controller
+            name='amount'
+            control={control}
+            render={({ field }) => (
+              <input
+                type="text"
+                id="amount"
+                value={field.value ? format.currencyInInput(field.value) : ''}
+                maxLength={18}
+                onChange={e => {
+                  let temp = e.target.value.replace(/[^0-9]/g, '')
+                  if (temp.length > 2) {
+                    temp = temp.slice(0, -2) + '.' + temp.slice(-2)
+                  }
+                  field.onChange(temp)
+                }}
+                className="border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm"
+              />
+
+            )}
+          />
+          {formState.errors.amount && (
+            <p className='text-sm text-red-400'>{formState.errors.amount.message}</p>
+          )}
+        </div>
+
+        <div className='space-y-2'>
+          <label className="block text-sm font-medium" htmlFor="description">Description</label>
+          <textarea
+            {...register('description')}
+            name="description"
+            id="description"
+            maxLength={200}
+            className="h-40 border border-zinc-800 rounded-lg px-3 py-2.5 bg-zinc-800/50 w-full text-sm"
+          ></textarea>
+          {formState.errors.description && (
+            <p className='text-sm text-red-400'>{formState.errors.description.message}</p>
+          )}
+        </div>
+
+        <div className="flex items-center justify-end gap-2">
+          <Button
+            type="submit"
+            className="bg-teal-400 text-teal-950 order-last"
+            disabled={formState.isSubmitting}
+          >
+            {formState.isSubmitting ? <Loader2 className='size-3 animate-spin' /> : <Check className="size-3" />}
+            Save
+          </Button>
+
+          <Dialog.Close asChild>
+            <Button>
+              <X className="size-3" />
+              Cancel
+            </Button>
+          </Dialog.Close>
+        </div>
+      </form>
+    </>
+  )
+}
