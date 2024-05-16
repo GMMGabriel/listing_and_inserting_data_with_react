5148739b8cff095c6ae2dd995355fa6cbdab9d02
Gabriel de Melo Marcondes <gabriel-santos1313@hotmail.com>
2024-05-16 00:01:22 -0300

    :sparkles: feat: US and BR symbols added

--- src/utilities/format.ts
+++ src/utilities/format.ts
@@ -1,62 +1,80 @@
 export class Format {
 
+  usSymbols = {
+    symbol: '$', // símbolo
+    decimal: '.', // separador de decimal
+    thousands: ',', // separador de milhares
+  }
+
+  ptBrSymbols = {
+    symbol: 'R$', // símbolo
+    decimal: ',', // separador de decimal
+    thousands: '.', // separador de milhares
+  }
+
   /**
    * @name currency
-   * @param {number | string} value valor recebido
-   * @param {boolean | undefined} withCurrencySymbol boolean para colocar o símbolo de dinheiro ou não
+   * @param {number | string} value valor recebido.
+   * @param {boolean | undefined} withCurrencySymbol boolean para colocar o símbolo de dinheiro ou não.
+   * @param {'us' | 'pt-br'} locale indica se o valor é em Dólar ou em Real.
    * @returns string
    */
-  currency(value: number | string, withCurrencySymbol: boolean = false): string {
-    if (value === '') return '0,00'
+  currency(value: number | string, withCurrencySymbol: boolean = false, locale: 'us' | 'pt-br' = 'us'): string {
+    const { symbol, decimal, thousands } = locale === 'us' ? this.usSymbols : this.ptBrSymbols
+
+    if (value === '') return `0${decimal}00`
 
     let slice = -2
     value = value.toString()
 
     if (value.includes(".")) {
-      value = value.replace(".", ",")
-      if (value.slice(slice, slice + 1) === ",") value += "0"
+      if (locale === 'pt-br')
+        value = value.replace(".", ",")
+      if (value.slice(slice, slice + 1) === decimal) value += "0"
     } else {
-      value = value + ",00"
+      value = value + `${decimal}00`
     }
     slice -= 4
 
     while (slice > -value.length) {
-      value = value.slice(0, slice) + "." + value.slice(slice)
+      value = value.slice(0, slice) + thousands + value.slice(slice)
       slice -= 4
     }
 
     if (withCurrencySymbol)
       return value.includes('-')
-        ? '- R$' + (value.replace('-', '')[0] === '.'
-          ? value.replace('-', '').replace('.', '')
+        ? `- ${symbol}` + (value.replace('-', '')[0] === thousands
+          ? value.replace('-', '').replace(thousands, '')
           : value.replace('-', ''))
-        : 'R$' + value
+        : `${symbol}` + value
     else
       return value
   }
 
   /**
    * @name currencyInInput
-   * @param {number | string} value valor recebido
-   * @param {boolean | undefined} withCurrencySymbol boolean para colocar o símbolo de dinheiro ou não
+   * @param {number | string} value valor recebido.
+   * @param {boolean | undefined} withCurrencySymbol boolean para colocar o símbolo de dinheiro ou não.
+   * @param {'us' | 'pt-br'} locale indica se o valor é em Dólar ou em Real.
    * @returns string
    */
-  currencyInInput(value: number | string, withCurrencySymbol: boolean = false): string {
+  currencyInInput(value: number | string, withCurrencySymbol: boolean = false, locale: 'us' | 'pt-br' = 'us'): string {
+    const { symbol, decimal, thousands } = locale === 'us' ? this.usSymbols : this.ptBrSymbols
     value = Number(value.toString().replace(/[^\d]/g, '')).toString()
 
-    if (value.length === 0) return `0,00`
-    if (value.length === 1) return `0,0${value}`
-    if (value.length === 2) return `0,${value}`
+    if (value.length === 0) return `0${decimal}00`
+    if (value.length === 1) return `0${decimal}0${value}`
+    if (value.length === 2) return `0${decimal}${value}`
 
     if (value.length > 14) value = value.slice(-14)
 
-    value = value.slice(0, -2) + ',' + value.slice(-2)
+    value = value.slice(0, -2) + decimal + value.slice(-2)
     let slice = -6
     while (slice > -value.length) {
-      value = value.slice(0, slice) + "." + value.slice(slice)
+      value = value.slice(0, slice) + thousands + value.slice(slice)
       slice -= 4
     }
 
-    return (withCurrencySymbol ? 'R$' : '') + value
+    return (withCurrencySymbol ? symbol : '') + value
   }
 }
